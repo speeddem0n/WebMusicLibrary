7a29e829ac44a2bcdb336d63950a84472e59132f
speeddem0n <slavik2890@gmail.com>
2025-01-16 01:24:52 +0300

    added env v to dockefile and entrypoint

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -23,8 +23,20 @@ CMD_ARGS=
 if [ ! -z ${DB_HOST} ]; then
   CMD_ARGS="${CMD_ARGS} -db-host ${DB_HOST}"
 fi
+if [ ! -z ${DB_PORT} ]; then
+  CMD_ARGS="${CMD_ARGS} -db-port ${DB_PORT}"
+fi
+if [ ! -z ${DB_USERNAME} ]; then
+  CMD_ARGS="${CMD_ARGS} -db-username ${DB_USERNAME}"
+fi
+if [ ! -z ${DB_PASSWORD} ]; then
+  CMD_ARGS="${CMD_ARGS} -db-password ${DB_PASSWORD}"
+fi
 if [ ! -z ${DB_SSL} ]; then
   CMD_ARGS="${CMD_ARGS} -db-ssl ${DB_SSL}"
+fi
+if [ ! -z ${API_URL} ]; then
+  CMD_ARGS="${CMD_ARGS} -api_url ${API_URL}"
 fi
 if [ ! -z ${LOG_LVL} ]; then
   CMD_ARGS="${CMD_ARGS} -log-lvl ${LOG_LVL}"
